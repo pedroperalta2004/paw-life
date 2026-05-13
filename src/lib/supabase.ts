cd48f2ea2662b1693ed113cd07085cfef03b4a8a
Pedro Peralta <a2022159880@alumni.iscac.pt>
2026-05-13 14:11:10 +0100

    Entrega Final (Atividade 3 - Fixed)

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,8 +2,8 @@ import "react-native-url-polyfill/auto";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://xfcjzhxgzffmsugzcvac.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhmY2p6aHhnemZmbXN1Z3pjdmFjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU2NTA3NzUsImV4cCI6MjA5MTIyNjc3NX0.GQYCMZEuWoF1Clj1q8zov-EcWvRvNhURfTavVDNZW4M";
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
